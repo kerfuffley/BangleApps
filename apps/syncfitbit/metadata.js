31d80d2326b63110c0c7e3d50f51b810347af6e0
kerfuffley <39776185+kerfuffley@users.noreply.github.com>
2024-10-11 12:56:47 -0500

    Create syncfitbit/metadata.js

---
+++ apps/syncfitbit/metadata.js
@@ -0,0 +1,13 @@
+{ "id": "syncfitbit",
+  "name": "Sync to Fitbit",
+  "shortName":"Sync to Fitbit",
+  "icon": "app.png",
+  "version":"0.01",
+  "description": "Uses Fitbit's API to automatically sync your steps to your Fitbit account.",
+  "tags": "health",
+  "supports": ["BANGLEJS2"],
+  "storage": [
+    {"name":"syncfitbit.app.js","url":"app.js"},
+    {"name":"syncfitbit.img","url":"app-icon.js","evaluate":true}
+  ]
+}
